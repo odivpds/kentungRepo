86b253a84b1e8b4a312b049b69c47410e829654b
agusprana <odhivpds01@gmail.com>
2026-06-25 00:27:03 +0800

    commit 2

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ const CONFIG = {
     // URL API Pusat (Server Utama Anda)
     API_BASE_URL: 'http://localhost/getfiles-api',
 
-    // URL Direct Link Pop-under (Adsterra, Monetag, dsb)
-    POPUNDER_URL_1: 'https://www.google.com/search?q=iklan-popunder-pertama',
-    POPUNDER_URL_2: 'https://www.google.com/search?q=iklan-popunder-kedua'
+    // URL Direct Link Pop-under Milik Client (Monetag, dsb)
+    CLIENT_POPUNDER_URL: 'https://www.google.com/search?q=iklan-popunder-client'
 };
